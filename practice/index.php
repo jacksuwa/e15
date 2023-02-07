84b740e4c55cfa10f791854b74072be5c3a50f42
Jackson Uwadiae <josaszz@gmail.com>
2023-02-07 22:07:53 +0000

    Added logo to practice app; modified readme file

--- practice/index.php
+++ practice/index.php
@@ -1,1 +1,15 @@
-<h1>Practice</h1>
+<!doctype html>
+<html lang='en'>
+
+<head>
+    <title>Practice App</title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+</head>
+
+<body>
+    <h1>Practice App</h1>
+    <img src='images/hes-logo.png'>
+</body>
+
+</html>
